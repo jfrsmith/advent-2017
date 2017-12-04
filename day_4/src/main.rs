44708f22cbc4684ec4d849a3ad3fdbe6e9158ed0
Jack Smith <jfrsmith@Jacks-MacBook-Pro.local>
2017-12-04 21:38:50 +0000

    parallelise line parsing

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,10 +1,17 @@
+#![feature(test)]
+
+extern crate test;
+extern crate rayon;
+
+use rayon::prelude::*;
+
 fn main() {
     println!("Part 1: {}", get_num_passphrases(include_str!("../input/input.txt"), false));
     println!("Part 2: {}", get_num_passphrases(include_str!("../input/input.txt"), true));
 }
 
 fn get_num_passphrases(passphrases: &str, with_anagrams: bool) -> usize {
-    passphrases.lines().filter(|passphrase| {
+    passphrases.par_lines().filter(|passphrase| {
         match with_anagrams {
             true => is_valid_passphrase_with_anagram(passphrase),
             false => is_valid_passphrase(passphrase)
@@ -52,4 +59,20 @@ fn test_2() {
     assert_eq!(is_valid_passphrase_with_anagram("a ab abc abd abf abj"), true);
     assert_eq!(is_valid_passphrase_with_anagram("iiii oiii ooii oooi oooo"), true);
     assert_eq!(is_valid_passphrase_with_anagram("oiii ioii iioi iiio"), false);
+}
+
+#[bench]
+fn bench_part_1(b: &mut test::Bencher) {
+    let input_str = include_str!("../input/input.txt");
+    b.iter(|| {
+        get_num_passphrases(input_str, false);
+    });
+}
+
+#[bench]
+fn bench_part_2(b: &mut test::Bencher) {
+    let input_str = include_str!("../input/input.txt");
+    b.iter(|| {
+        get_num_passphrases(input_str, true);
+    });
 }
