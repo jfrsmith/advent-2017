4230d27ade6166b6bc5a42cb89e90b80847544dc
Jack Smith <jfrsmith@Jacks-MacBook-Pro.local>
2017-12-04 21:08:20 +0000

    day_4 complete

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,10 +1,14 @@
 fn main() {
-    println!("Part 1: {}", get_num_passphrases(include_str!("../input/input.txt")));
+    println!("Part 1: {}", get_num_passphrases(include_str!("../input/input.txt"), false));
+    println!("Part 2: {}", get_num_passphrases(include_str!("../input/input.txt"), true));
 }
 
-fn get_num_passphrases(passphrases: &str) -> usize {
+fn get_num_passphrases(passphrases: &str, with_anagrams: bool) -> usize {
     passphrases.lines().filter(|passphrase| {
-        is_valid_passphrase(passphrase)
+        match with_anagrams {
+            true => is_valid_passphrase_with_anagram(passphrase),
+            false => is_valid_passphrase(passphrase)
+        }
     }).collect::<Vec<&str>>().len()
 }
 
@@ -18,9 +22,34 @@ fn is_valid_passphrase(passphrase: &str) -> bool {
     num_vals == passphrase_vec.len()
 }
 
+fn is_valid_passphrase_with_anagram(passphrase: &str) -> bool {
+    let passphrase_vec : Vec<&str> = passphrase.split_whitespace().collect();
+    let num_vals = passphrase_vec.len();
+    
+    let mut sorted_passphrase_vec = passphrase_vec.into_iter().map(|x| {
+        let mut chars : Vec<char> = x.chars().collect();
+        chars.sort();
+        chars.into_iter().collect::<String>()
+    }).collect::<Vec<String>>();
+
+    sorted_passphrase_vec.sort();
+    sorted_passphrase_vec.dedup();
+
+    num_vals == sorted_passphrase_vec.len()
+}
+
 #[test]
 fn test_1() {
     assert_eq!(is_valid_passphrase("aa bb cc dd ee"), true);
     assert_eq!(is_valid_passphrase("aa bb cc dd aa"), false);
     assert_eq!(is_valid_passphrase("aa bb cc dd aaa"), true);
+}
+
+#[test]
+fn test_2() {
+    assert_eq!(is_valid_passphrase_with_anagram("abcde fghij"), true);
+    assert_eq!(is_valid_passphrase_with_anagram("abcde xyz ecdab"), false);
+    assert_eq!(is_valid_passphrase_with_anagram("a ab abc abd abf abj"), true);
+    assert_eq!(is_valid_passphrase_with_anagram("iiii oiii ooii oooi oooo"), true);
+    assert_eq!(is_valid_passphrase_with_anagram("oiii ioii iioi iiio"), false);
 }
